f76baf5740451d2416da6780c50af0964640777b
Major Hayden <major@mhtx.net>
2023-06-20 16:13:12 -0500

    Fix spec filename

---
+++ specmaker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Generate spec files for submodules in Google's Cloud Go package.
+
+for SUBMODULE in $(ls -d */ | sed 's/\/$//'); do 
+    GIT_TAG=$(git tag | sort --version-sort -r | grep $SUBMODULE | head -n 1)
+    if [ -z $GIT_TAG ]; then
+        continue
+    fi
+
+    VERSION=$(echo $GIT_TAG | sed 's/[a-z0-9]*\/v//')
+    GIT_TAG_SPEC=$(echo $GIT_TAG | sed "s/$VERSION/%{version}/")
+    echo -e "$SUBMODULE: \t$GIT_TAG_SPEC ($VERSION)"
+
+    export VERSION SUBMODULE
+
+    mkdir -vp specs
+    envsubst < ../template.spec > ../specs/golang-google-cloud-$SUBMODULE.spec
+done
